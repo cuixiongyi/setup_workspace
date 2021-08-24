8b20aa96c427ab03e7d987437f9f571d9863664e
Xiongyi Cui <cuixiongyi2013@gmail.com>
2021-08-24 17:19:18 -0400

    fix C-r

--- setup_instance.sh
+++ setup_instance.sh
@@ -44,9 +44,9 @@ echo "# xiongyi workspace setup script start----------
                 # ensure synchronization between Bash memory and history file
                 export PROMPT_COMMAND=\"history -a; history -n; ${PROMPT_COMMAND}\"
                 # if this is interactive shell, then bind hstr to Ctrl-r (for Vi mode check doc)
-                if [[ $- =~ .*i.* ]]; then bind '\"\C-r\": \"\C-a hstr -- \C-j\"'; fi
+                if [[ \$- =~ .*i.* ]]; then bind '\"\C-r\": \"\C-a hstr -- \C-j\"'; fi
                 # if this is interactive shell, then bind 'kill last command' to Ctrl-x k
-                if [[ $- =~ .*i.* ]]; then bind '\"\C-xk\": \"\C-a hstr -k \C-j\"'; fi
+                if [[ \$- =~ .*i.* ]]; then bind '\"\C-xk\": \"\C-a hstr -k \C-j\"'; fi
                 #export HSTR_CONFIG=blacklist
 # xiongyi workspace setup script end----------
 " | tee -a ~/.bashrc
