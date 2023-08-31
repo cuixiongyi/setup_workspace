c903777eb64452b8f3f6eba159009f0ece0eb6ad
Xiongyi Cui <cuixiongyi2013@gmail.com>
2023-08-31 17:37:58 -0400

    Update setup_instance.sh

--- setup_instance.sh
+++ setup_instance.sh
@@ -74,16 +74,16 @@ echo "# xiongyi workspace setup script start----------
                 alias hh=hstr                    # hh to be alias for hstr
                 export HSTR_CONFIG=hicolor,prompt-bottom,keywords-matching  # get more colors, show prompt at bottom, reg based matching
                 shopt -s histappend              # append new history items to .bash_history
-        	# export HISTCONTROL=ignorespace   # leading space hides commands from history
-                export HISTFILESIZE=\${HISTSIZE}        # increase history file size (default is 500)
-                export HISTSIZE=\${HISTFILESIZE}  # increase history size (default is 500)
-                # ensure synchronization between Bash memory and history file
-                export PROMPT_COMMAND=\"history -a; history -n; ${PROMPT_COMMAND}\"
-                # if this is interactive shell, then bind hstr to Ctrl-r (for Vi mode check doc)
-                if [[ \$- =~ .*i.* ]]; then bind '\"\C-r\": \"\C-a hstr -- \C-j\"'; fi
-                # if this is interactive shell, then bind 'kill last command' to Ctrl-x k
-                if [[ \$- =~ .*i.* ]]; then bind '\"\C-xk\": \"\C-a hstr -k \C-j\"'; fi
-                #export HSTR_CONFIG=blacklist
+        	# # export HISTCONTROL=ignorespace   # leading space hides commands from history
+         #        export HISTFILESIZE=\${HISTSIZE}        # increase history file size (default is 500)
+         #        export HISTSIZE=\${HISTFILESIZE}  # increase history size (default is 500)
+         #        # ensure synchronization between Bash memory and history file
+         #        export PROMPT_COMMAND=\"history -a; history -n; ${PROMPT_COMMAND}\"
+         #        # if this is interactive shell, then bind hstr to Ctrl-r (for Vi mode check doc)
+         #        if [[ \$- =~ .*i.* ]]; then bind '\"\C-r\": \"\C-a hstr -- \C-j\"'; fi
+         #        # if this is interactive shell, then bind 'kill last command' to Ctrl-x k
+         #        if [[ \$- =~ .*i.* ]]; then bind '\"\C-xk\": \"\C-a hstr -k \C-j\"'; fi
+         #        #export HSTR_CONFIG=blacklist
         
         # Set the default python debugger to be pudb
         export PYTHONBREAKPOINT="pudb.set_trace"
